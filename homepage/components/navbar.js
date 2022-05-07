c46057c1b93d3feaa798f0ed74071d0f24ffdebc
Srinivas-Bhat <srinivaskbhat840@gmail.com>
2022-05-07 13:08:27 +0530

    adding Landing Page again

---
+++ homepage/components/navbar.js
@@ -0,0 +1,132 @@
+const container = () => {
+  return ` <div id="offer-section">
+  <div></div>
+  <div class="offer-info">
+      <p><span id="span1">Limited time only, online only! </span><span id="span2">$40 SPRING GIFT BUNDLE WITH $40 ORDER     </span> <span id="span3"><a href="#">DETAILS</a></span></p>
+  </div>
+  <div></div>
+</div>
+<div id="store_pickup-section">
+  <div id="pickup-bystore">
+    <p>
+      <span><i class="fa-solid fa-location-dot pickup"></i></span
+      ><span class="message">PICK UP IN STORE</span
+      ><span><a href="#" class="setstore">Set Store</a></span>
+    </p>
+  </div>
+</div>
+<div id="navbar">
+  <div id="options-section">
+    <div id="website-title">
+      <div id="random">
+        <div class="form-group fg--search">
+          <input type="text" class="input" placeholder="Search by fragrance or products..." />
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </div>
+        <div id="userImage-div">
+          <img
+            src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwc807e833/images/svg-icons/UI-MyAccount.svg?yocs=o_s_"
+            alt=""
+            id="user-image"
+          />
+        </div>
+        <div id="userCart-div">
+          <i class="fa-solid fa-bag-shopping usercart"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="hovering-section">
+      <div class="hoveringUL">
+          <p class="hovering-ptag1">NEW & NOW</p>
+          <p class="hovering-ptag2">BODY CARE</p>
+          <p class="hovering-ptag3">HAND SOAPS & SANITIZERS</p>
+          <p class="hovering-ptag4">HOME FRAGRANCE</p>
+          <p class="hovering-ptag5">GIFTS</p>
+          <p class="hovering-ptag6">TOP OFFERS</p>
+      </div>
+      <div id="new-and-now">
+        <div class="left">
+          <p>NEW & NOW</p>
+          <p>EXPLORE NEW & NOW</p>
+        </div>
+        <div class="right">
+          <p>New Arrivals</p>
+          <p>Men's Shop</p>
+          <p>Laundry Fragrances</p>
+          <p>Best Sellers</p>
+          <p>Aromatherapy Shop</p>
+        </div>
+      </div>
+    <div id="body-care">
+      <div class="left">
+        <p>FEATURED</p>
+        <p>BATH & SHOWER</p>
+        <p>MOISTURIZERS</p>
+        <p>FRAGRANCE</p>
+        <p>AROMATHERAPY</p>
+        <p>MEN'S</p>
+        <p>GIFT SETS</p>
+        <p>EXPLORE BODY CARE</p>
+      </div>
+      <div class="right">
+        <p>Travel Size</p>
+        <p>Retired Fragrance</p>
+        <p>New body Care</p>
+        <p>All Bath & Shower</p>
+        <p>Body Cream</p>
+        <p>All Men's</p>
+      </div>
+    </div>
+    <div id="sanitizers">
+      <div class="left">
+        <p>FEATURED</p>
+        <p>HAND SOAPS</p>
+        <p>HAND SANITIZERS</p>
+        <p>GIFT SETS</p>
+        <p>EXPLORE HAND SOAP & SANITIZERS</p>
+      </div>
+      <div class="right">
+        <p>New Hand Soaps & Sanitizers</p>
+        <p>All Hand Soaps</p>
+        <p>Full Size Sanitizers</p>
+        <p>Hand Soap & Sanitizers Gifts Sets</p>
+      </div>
+    </div>
+    <div id="fragrance">
+      <div class="left">
+        <p>FEATURED</p>
+        <p>CANDLES</p>
+        <p>AIR FRESHNERS</p>
+        <p>EXPLORE HOME FRAGRANCE</p>
+      </div>
+      <div class="right">
+        <p>New Home Fragrance</p>
+        <p>3-Wick Candles</p>
+        <p>Candles</p>
+        <p>Wall Flowers Plug</p>
+      </div>
+    </div>
+    <div id="gifts">
+      <div class="left">
+        <p>FEATURED</p>
+        <p>GIFTS BY PRICE</p>
+        <p>GIFTS BY RECIPIENT</p>
+        <p>COLLECTIONS</p>
+        <p>GIFT CARDS</p>
+        <p>EXPLORE GIFTS</p>
+      </div>
+      <div class="right">
+        <p>Mother's Day Gifts</p>
+        <p>Father's Day Gifts</p>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>`;
+};
+
+
+export {container};
+
+
